14e4107f2d6558c0f078e97fe671dd112cff64e3
Arthur Shmigelski <arthur19shmigelski@gmail.com>
2021-08-30 19:44:56 +0300

    Commit_2_resave

---
+++ Finally/School/School.MVC/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using School.BLL.Models.Enum;
+
+namespace School.MVC.Models
+{
+    public class StudentModel : PersonModel
+    {
+        public int? GroupId { get; set; }
+        public StudentGroupModel Group { get; set; }
+        public StudentType Type { get; set; }
+
+    }
+}
